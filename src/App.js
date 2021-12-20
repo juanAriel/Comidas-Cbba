f797d26f84ee07bd28291871bf3cddbde090c2b1
juanAriel <arielvia2013@gmail.com>
2021-12-20 19:28:56 -0400

    hasta aqui se terino lo que es la plantilla hero que contiene una publicacion de oferta y compra

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar';
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
        <NavBar />
+       <Hero/>
     </>
   );
 }
