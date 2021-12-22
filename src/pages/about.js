32cdb433ba86f52e4b6ce9f36352b1479d4a3db6
juanAriel <arielvia2013@gmail.com>
2021-12-21 23:12:22 -0400

    hasta aqui se termino un poco de las rutas y de las pages que va a contener nuestro ecomerce

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function about() {
+    return (
+        <div className='h-screen flex justify-center items-center bg-yellow-300'>
+            <h1 className='text-9xl uppercase font-black'>
+                About Page
+            </h1>
+        </div>
+    )
+}
