9ee49e224fc971c004ffcc70dcdd93dfb7007e31
juanAriel <arielvia2013@gmail.com>
2022-01-18 20:51:30 -0400

    hasta aqui se termino la seccion Acerca de  el cual consta de una imagen y una pequeña descripcion todo con estilos de tailwind

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -9,25 +9,25 @@ export default function Content() {
           <div className="menu-card">
               <img src={Desayuno} alt="" className="h-48 rounded mb-10 shadow md:h-full" />
               <div className="flex flex-col justify-center items-center">
-                  <h2 className="text-2xl-mb-2">Api con pastel</h2>
-                  <p className="mb-2 text-center">Desayuno tradicional de la ciudad de Cochabamba</p>
-                  <span>10bs</span>
+                  <h2 className="text-2xl-mb-2 font-black uppercase">Api con pastel</h2>
+                  <p className="mb-2 text-center font-black">Desayuno tradicional de la ciudad de Cochabamba</p>
+                  <span className='text-3xl font-black'>10bs</span>
               </div>
           </div>
           <div className="menu-card">
               <img src={Almuerzo} alt="" className="h-48 rounded mb-10 shadow md:h-full" />
               <div className="flex flex-col justify-center items-center">
-                  <h2 className="text-2xl-mb-2">Sopa de mani</h2>
-                  <p className="mb-2 text-center">Sopa tradicional de la ciudad de Cochabamba con ingedientes base como mani, fideo y papas fritas</p>
-                  <span>12bs</span>
+                  <h2 className="text-2xl-mb-2 font-black uppercase">Sopa de mani</h2>
+                  <p className="mb-2 text-center font-black">Sopa tradicional de la ciudad de Cochabamba con ingedientes base como mani, fideo y papas fritas</p>
+                  <span className='text-3xl font-black'>12bs</span>
               </div>
           </div>
           <div className="menu-card">
               <img src={Cena} alt="" className="h-48 rounded mb-10 shadow md:h-full" />
               <div className="flex flex-col justify-center items-center">
-                  <h2 className="text-2xl-mb-2">Silpancho</h2>
-                  <p className="mb-2 text-center">Plato que se frecuenta en de Cochabamba</p>
-                  <span>15bs</span>
+                  <h2 className="text-2xl-mb-2 font-black uppercase">Silpancho</h2>
+                  <p className="mb-2 text-center font-black">Plato que se frecuenta en de Cochabamba</p>
+                  <span className='text-3xl font-black'>15bs</span>
               </div>
           </div>  
         </>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
 export default function about() {
+    const img = require('../images/aboust-us.png');
     return (
         <div className='h-screen flex justify-center items-center bg-yellow-300'>
-            <h1 className='text-9xl uppercase font-black'>
-                About Page
-            </h1>
+                <div className='grid lg:grid-cols-2 py-20 max-w-7xl mx-auto'>
+                    <div className="py-10 px-10">
+                        <h1 className="text-6xl font-black">Acerca de</h1>
+                        <p className="py-10 text-base">Estudié Ingenieria de Sistemas con poca experiencia en el mundo del desarrollo me anime a tomar un BootCamp en Tzuzul y con esta pequeña App trato de demostrar todo lo apredido.
+                        Me gusta incurcionar en diferentes sectores relacionado con el desarrollo de software y me encantan los nuevos retos.
+                        si te interesaria conocer mas sobre mi, el desarrollo y/o implementacion de esta App no dudes en dejarnos tu mensaje en el apartado de.</p>
+                          
+                   <a href='/contacto' class="bg-blue-500    hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">Contactos</a> 
+                    </div>
+                    <div className="hero-image-container w-4/6 mx-auto" >
+                            <img className='bg-transparent rounded-full' src={img}></img>   
+                    </div>
+                </div>
         </div>
     )
 }
